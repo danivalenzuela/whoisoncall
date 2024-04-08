979c8808653732983e6c582e417b6fea0f0f0170
Daniel Valenzuela <139027124+danivalenzuela@users.noreply.github.com>
2024-04-08 14:32:08 -0300

    Update popup.js
    clean code

--- popup.js
+++ popup.js
@@ -1,23 +1,19 @@
-const apiKey = 'Your_api_key_opsgenie'; 
+const apiKey = 'Your_apikey';
 const baseUrl = 'https://api.opsgenie.com';
-//const tagIncidente = 'Your_filter_tagsIncident_opsgenie';
-
-let entradasAnteriores = []; 
 
 async function obtenerIncidentesAbiertosConTag(tag) {
     try {
-        const query = encodeURIComponent(`status: open AND tag:XXXXXXX`);
+        const query = encodeURIComponent(`status: open AND tag: "${tag}"`);
         const response = await fetch(`${baseUrl}/v1/incidents?query=${query}`, {
             headers: {
                 Authorization: `GenieKey ${apiKey}`
             }
         });
-
         const data = await response.json();
-        return data.data; // Retorna los datos de los incidentes
+        return data.data;
     } catch (error) {
         console.error('Error al obtener los incidentes abiertos:', error);
-        return []; // Retorna un array vacío en caso de error
+        return [];
     }
 }
 
@@ -26,10 +22,8 @@ async function listarEntradasTimelineIncidente(incidentId) {
         const response = await fetch(`${baseUrl}/v2/incident-timelines/${incidentId}/entries`, {
             headers: {
                 Authorization: `GenieKey ${apiKey}`
-            },
-            
+            }
         });
-
         return response.json();
     } catch (error) {
         console.error('Error al listar resultados:', error);
@@ -43,30 +37,22 @@ async function obtenerYMostrarIncidentesAbiertosConTag(tag) {
         const resultadosDiv = document.getElementById('resultados');
 
         if (incidentes.length === 0) {
-            const noResultsP = document.createElement('p');
-            noResultsP.textContent = '0 ECAS Abiertos! '; 
-            resultadosDiv.appendChild(noResultsP);
+            resultadosDiv.textContent = '0 ECAS Abiertos! ';
         } else {
             incidentes.forEach(async incidente => {
                 const incidenteDiv = document.createElement('div');
                 incidenteDiv.classList.add('incidente');
-
                 const mensajeP = document.createElement('p');
                 mensajeP.textContent = `Incidente: ${incidente.message}`;
                 incidenteDiv.appendChild(mensajeP);
-
                 const timeline = await listarEntradasTimelineIncidente(incidente.id);
                 if (timeline) {
                     const eventosUl = document.createElement('ul');
                     timeline.data.entries.forEach(entry => {
                         if (entry.title.content === 'Responder alert acked') {
-                            // Verificar si la entrada es nueva
-                            if (!entradasAnteriores.includes(entry.id)) {
-                                const eventoLi = document.createElement('li');
-                                eventoLi.textContent = `Se contacto a: ${entry.actor.name}, Status alerta: ${entry.title.content}`;
-                                eventosUl.appendChild(eventoLi);
-                                entradasAnteriores.push(entry.id); 
-                            }
+                            const eventoLi = document.createElement('li');
+                            eventoLi.textContent = `Se contacto a: ${entry.actor.name}, Status alerta: ${entry.title.content}`;
+                            eventosUl.appendChild(eventoLi);
                         }
                     });
                     incidenteDiv.appendChild(eventosUl);
@@ -75,10 +61,10 @@ async function obtenerYMostrarIncidentesAbiertosConTag(tag) {
             });
         }
     } catch (error) {
-        console.error(`Error al obtener resultados`, error);
+        console.error(`Error al obtener y mostrar los incidentes abiertos con la etiqueta "${tag}":`, error);
     }
 }
 
-//window.onload = function() {
-   // obtenerYMostrarIncidentesAbiertosConTag(tagIncidente);
+window.onload = function() {
+    obtenerYMostrarIncidentesAbiertosConTag('NOC-Incidents-Test');
 };
